0a281f7c325ef982df9c3463e80df32edbff9627
ojas <ojas24012002jain@gmail.com>
2021-03-31 02:54:02 +0530

    animations

--- app/src/main/java/com/example/chatappv0/allusersActivity.java
+++ app/src/main/java/com/example/chatappv0/allusersActivity.java
@@ -2,6 +2,8 @@ package com.example.chatappv0;
 
 import android.content.Intent;
 import  android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.chatappv0.Adapter.AllUsersAdapter;
 import com.example.chatappv0.Models.usersModel;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,19 +27,26 @@ import java.util.ArrayList;
 public class allusersActivity extends AppCompatActivity {
     private AllUsersAdapter allUsersAdapter;
     private RecyclerView recyclerView;
-
+    private LottieAnimationView empty,loading;
+    private TextView noFriends,loadingText;
     ArrayList<usersModel> userList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_allusers);
+        noFriends=findViewById(R.id.noFriendsTextaau);
+        empty=findViewById(R.id.emptyaau);
+        loading=findViewById(R.id.loadingaau);
+        loadingText=findViewById(R.id.loadingTextaau);
         recyclerView=findViewById(R.id.userslist);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         readusers();
     }
     private void readusers() {
+        loading.setSpeed(1);
+        loading.playAnimation();
         final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference("users");
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -53,9 +63,12 @@ public class allusersActivity extends AppCompatActivity {
                                 if(!snapshot.hasChild(user.getUserid())) {
                                     userList.add(user);
                                 }
+                                loadingText.setVisibility(View.INVISIBLE);
+                                loading.setVisibility(View.INVISIBLE);
                                 allUsersAdapter = new AllUsersAdapter(getApplicationContext(), userList);
                                 recyclerView.setLayoutManager(new LinearLayoutManager(allusersActivity.this));
                                 recyclerView.setAdapter(allUsersAdapter);
+                                EmptyListAnimation();
                             }
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) {
@@ -78,5 +91,16 @@ public class allusersActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(allusersActivity.this, MainActivity.class));
+    }
+    private void EmptyListAnimation() {
+        if (userList.isEmpty()) {
+            empty.setVisibility(View.VISIBLE);
+            noFriends.setVisibility(View.VISIBLE);
+            empty.setSpeed(1);
+            empty.playAnimation();
+        } else {
+            empty.setVisibility(View.GONE);
+            noFriends.setVisibility(View.GONE);
+        }
     }
 }
